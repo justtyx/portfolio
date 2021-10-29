69e3c4f27cbd1430eceefc16c2b7f92c7e8e3947
Justina Jemeljanovaite <just1528@stud.kea.dk>
2021-10-29 21:30:37 +0200

    sliding out menu

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,19 @@
 const menuBtn = document.querySelector('.hamburger');
 
 let menuOpen = false;
+
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen) {
+        // menuBtn.classList.add('open');
+        document.querySelector('nav > ul.menu-items#menu-items').style.display = 'flex';
+        document.querySelector('nav > ul.menu-items').classList.add('slide-in');
+        menuOpen = true;
+    } else {
+        document.querySelector('nav > ul.menu-items').classList.add('slide-out');
+        console.log('closed');
+        setTimeout(()=> {
+            document.querySelector('nav > ul.menu-items#menu-items').style.display = 'none';
+        }, 200);
+        menuOpen = false;
+    }
+});
