c67dbae76281ab77954836bc13e44b4151cb91b7
Justina Jemeljanovaite <just1528@stud.kea.dk>
2021-10-29 22:02:02 +0200

    commit

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,18 @@
 const menuBtn = document.querySelector('.hamburger');
+const hamburgerFirst = document.querySelector('.line:first-child');
+const  hamburgerSecond = document.querySelector('.line:nth-child(2)');
+const  hamburgerThird = document.querySelector('.line:last-child');
 
 let menuOpen = false;
 
 menuBtn.addEventListener('click', () => {
     if(!menuOpen) {
-        // menuBtn.classList.add('open');
+        menuBtn.classList.add('open');
         document.querySelector('nav > ul.menu-items#menu-items').style.display = 'flex';
         document.querySelector('nav > ul.menu-items').classList.add('slide-in');
         menuOpen = true;
     } else {
+        menuBtn.classList.remove('open');
         document.querySelector('nav > ul.menu-items').classList.add('slide-out');
         console.log('closed');
         setTimeout(()=> {
