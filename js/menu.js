c85011d186bf125d11114cc2552e452779ed2439
Justina Jemeljanovaite <just1528@stud.kea.dk>
2021-10-30 23:42:14 +0200

    finalised hamburger menu

--- js/menu.js
+++ js/menu.js
@@ -1,19 +1,6 @@
 const navToggle = document.querySelector('.hamburger');
 const menuToggle = document.querySelector('#menu-items');
-const menuLines = document.querySelectorAll('.line');
 
 navToggle.addEventListener('click', () => {
   document.body.classList.toggle('nav-open');
 });
-
-navToggle.addEventListener('mouseover', () => {
-    menuLines[0].style.background = '#131313';
-    menuLines[1].style.background = '#131313';
-    menuLines[2].style.background = '#131313';
-})
-
-navToggle.addEventListener('mouseout', () => {
-    menuLines[0].style.background = 'transparent';
-    menuLines[1].style.background = 'transparent';
-    menuLines[2].style.background = 'transparent';
-})
