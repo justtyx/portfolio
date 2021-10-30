98408c3a773c8c2b5e3b9f3b192f4bd68bbd170b
Justina Jemeljanovaite <just1528@stud.kea.dk>
2021-10-31 00:54:32 +0200

    removed nav js

--- js/nav.js
+++
@@ -1,9 +0,0 @@
-let header = document.querySelector('header')
-
-window.addEventListener('scroll', () => {
-  header.classList.toggle('sticky', window.scrollY > 0)
-})
-
-window.addEventListener('scroll', () => {
-    header.classList.toggle('sticky', window.scrollY > 0)
-  })
